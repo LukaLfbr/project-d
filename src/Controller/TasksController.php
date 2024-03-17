67ef9bf26a54cadd2e4804bb13acfd8c785ad713
Luka <luka.lefebvrepro@gmail.com>
2024-03-17 19:10:44 +0100

    Ajoute de la méchanique de suppression et d'ajout

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -3,9 +3,13 @@
 namespace App\Controller;
 
 use App\Repository\TasksRepository;
+use App\Entity\Tasks;
+use App\Form\TasksType;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,16 +22,16 @@ class TasksController extends AbstractController
         $tasks = $repository->findAll();
 
         // Si besoin de modifier la description manuellement
-        $tasks[0]->setDescription('Aller faire des petites couses pour la semaine');
+        // $tasks[0]->setDescription('Aller faire des petites couses pour la semaine');
 
         // Si besoin de modifier la date manuellement
-        $limitDateSetter = DateTime::createFromFormat('Y-m-d', '2024-03-20');
-        $tasks[0]->setLimitDate($limitDateSetter);
+        // $limitDateSetter = DateTime::createFromFormat('Y-m-d', '2024-03-20');
+        // $tasks[0]->setLimitDate($limitDateSetter);
 
         // Si besoin de remove
         // $em->remove($tasks[x])
 
-        $em->flush();
+        // $em->flush();
 
         return $this->render('tasks/tasks.html.twig', [
             'controller_name' => 'TasksController',
@@ -48,5 +52,36 @@ class TasksController extends AbstractController
             'tasks' => $tasks
 
         ]);
+    }
+
+    #[Route('/tasks/addtask', name: 'addtask')]
+    public function addTask(Request $request, EntityManagerInterface $em): Response
+    {
+
+        $task = new Tasks();
+        // Associe les données du formulaire aux données dans l'objet task
+        $form = $this->createForm(TasksType::class, $task);
+        // Récupère les données dans l'url
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() and $form->isValid()) {
+            $em->persist($task);
+            $em->flush();
+            return $this->redirectToRoute('app_tasks');
+        }
+
+        return $this->render('/tasks/addTasks.html.twig', [
+
+            'form' => $form,
+        ]);
+    }
+
+    #[Route("/tasks/delete{id}", name: "deleteTask")]
+    public function deleteTask(Tasks $task, EntityManagerInterface $em): Response
+    {
+        $em->remove($task);
+        $em->flush();
+
+        return $this->redirectToRoute('app_tasks');
     }
 }
