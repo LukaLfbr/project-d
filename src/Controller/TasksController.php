f00b02fa3e0f6114af90b850492e0be810d0ca1e
Luka <luka.lefebvrepro@gmail.com>
2024-03-17 16:05:06 +0100

    Ajout entre deux de la EntityManagerInterface pour changer manuellement les valeurs plus tard si besoin.
    +du use DateTime pour le setter limit_date

--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Repository\TasksRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +12,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class TasksController extends AbstractController
 {
     #[Route('/tasks', name: 'app_tasks')]
-    public function index(TasksRepository $repository): Response
+    public function index(TasksRepository $repository, EntityManagerInterface $em): Response
     {
 
         $tasks = $repository->findAll();
 
+        // Si besoin de modifier la description manuellement
+        $tasks[0]->setDescription('Aller faire des petites couses pour la semaine');
+
+        // Si besoin de modifier la date manuellement
+        $limitDateSetter = DateTime::createFromFormat('Y-m-d', '2024-03-20');
+        $tasks[0]->setLimitDate($limitDateSetter);
+
+        // Si besoin de remove
+        // $em->remove($tasks[x])
+
+        $em->flush();
 
         return $this->render('tasks/tasks.html.twig', [
             'controller_name' => 'TasksController',
